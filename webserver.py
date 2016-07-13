b34c42eec69b5473d10889d0c1a701400e03ad86
shomislav <lukovicm@gmail.com>
2016-07-13 22:14:34 +0200

    Dodato tabelarno ispisivanje podataka.

--- webserver.py
+++ webserver.py
@@ -4,23 +4,51 @@ import threading
 import time
 
 app = Flask(__name__)
-
 @app.route("/")
 def hello():
     return "Hello Worddddld!"
 
+@app.route("/get_data")
+def get_data():
+    data.get_data()
+    #print data.prices
+    return "%s" % data.prices
+
 @app.route("/table")
 def table():
-    return "...table html..."
+    tableHTML = ""
+    for e in reversed(data.prices):
+        tableHTML += '''
+        <tr>
+            <td>%s</td>
+            <td>%s</td>
+            <td>%s</td>
+        </tr>
+        ''' %e
+    tabela = '''
+    <html>
+	<head>
+	<meta http-equiv="refresh" content="5">
+	</head>
+    <body>
+    <TABLE  BORDER="5"; WIDTH=50%%>
+   <TR>
+      <TH COLSPAN="3">
+         <H3><BR>Podaci s burza</H3>
+      </TH>
+   </TR>
+      <TH>Symbol</TH>
+      <TH>Date</TH>
+      <TH>Price</TH>
+   <TR>
+    %s
+   </TR>
+</TABLE>
+    </body>
+    </html>
+    '''
 
-@app.route("/graph")
-def graph():
-    return "...graph html..."
-
-@app.route("/getdata")
-def getdata():
-    data.get_data()
-    return "OK\n"
+    return tabela % tableHTML
 
 @app.route("/data")
 def returndata():
@@ -36,15 +64,23 @@ def returndata():
     '''
     return s % data.prices
 
-def poller():
+@app.route("/graph")
+def graph():
+    return "...graph html..."
+
+
+def puller():
     while True:
+        print "polling"
         data.get_data()
         time.sleep(10)
 
-t = threading.Thread(target=poller)
+
+t = threading.Thread(target=puller)
 t.setDaemon(True)
 t.start()
 
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
 
